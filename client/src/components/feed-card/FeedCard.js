0f4df68c5a04348661f636b76ad5845a001fafdc
dianerdiana9 <dianerdiana.de@gmail.com>
2022-02-07 00:04:34 +0700

    post-comment

--- client/src/components/feed-card/FeedCard.js
+++ client/src/components/feed-card/FeedCard.js
@@ -1,6 +1,12 @@
-import React from "react"
-import { Modal, Container, Row, Col, Image } from 'react-bootstrap'
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react"
+import { Modal, Container, Row, Col, Image, Form, Button } from 'react-bootstrap'
+import { Link, useNavigate } from "react-router-dom";
+
+import { API } from "../../config/api"
+
+import { useState, useContext } from 'react';
+import { UserContext } from '../../context/userContext';
+
 import style from "./FeedCard.module.css"
 
 function FeedCard (props) {
@@ -32,6 +38,7 @@ function FeedCard (props) {
             </button>
 
             <Detail
+              detailFeed = {props.item}
               show={detailFeed}
               onHide={() => setDetailFeed(false)}
             />
@@ -49,6 +56,78 @@ function FeedCard (props) {
 }
 
 function Detail(props) {
+
+  const navigate = useNavigate()
+
+  const [state, dispatch] = useContext(UserContext);
+
+  let feed= props.detailFeed
+
+  const [ comments, setComments ] = useState();
+  const [ form, setForm ] = useState({
+    comment: "",
+    feed_id: feed.id,
+    user_id: state.user.id
+  })
+
+  const getComments = async(feedId) => {
+    try {
+
+      const response = await API.get("/comments/" + feedId)
+      
+      setComments(response.data.data.comments)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async(e) => {
+    try {
+
+      e.preventDefault();
+
+      if(e.key === "Enter") {
+
+       setForm({
+          feed_id: form?.feed_id,
+          comment: form?.comment,
+          user_id: form?.user_id
+        })
+      }
+      
+      const config = {
+        headers: {
+          'Content-type': 'application/json',
+        },
+      };
+
+      const body = JSON.stringify(form);
+
+      const response = await API.post("/comment", body, config)
+
+      console.log(response.data)
+
+      if(response.status == "Success") {
+        navigate("/feed")
+      }
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getComments(feed.id)
+  }, []);
+
   return (
     <Modal
       {...props}
@@ -61,7 +140,7 @@ function Detail(props) {
         <Container style={{padding: "0"}}>
           <Row>
             <Col md={8} style={{overflowY: "hidden", height: "90vh"}}>
-              <Image src="/images/large.jpg" style={{width: "100%", height: "100%", objectFit: "cover", borderRadius: "5px"}}/>
+              <Image src={feed.fileName} style={{width: "100%", height: "100%", objectFit: "cover", borderRadius: "5px"}}/>
             </Col>
             <Col md={4}>
               <div>
@@ -71,33 +150,44 @@ function Detail(props) {
               </div>
               <div style={{display: "flex", flexDirection: "column", borderBottom: "1px #ABABAB solid", marginRight: "20px", padding: "20px 0px"}}>
                 <div>
-                  <Image src="/images/zayn.png" style={{width: "30px", height: "30px", borderRadius: "50%"}}/>
-                  <span style={{color: "white", marginLeft: "10px"}}>zayn</span>
+                  <Image src={feed.uploader.image} style={{width: "30px", height: "30px", borderRadius: "50%"}}/>
+                  <span style={{color: "white", marginLeft: "10px"}}>{feed.uploader.fullName}</span>
                 </div>
                 <div style={{marginLeft: "15px", color: "#FFFFFF", flexWrap: "wrap"}}>
-                  <div style={{color: "#ABABAB", marginLeft: "25px"}}>To Begin Again ...</div>
+                  <div style={{color: "#ABABAB", marginLeft: "25px"}}>{feed.caption}</div>
                 </div>
               </div>
-              <div style={{display: "flex", flexDirection: "column", marginRight: "20px", marginTop: "20px"}}>
-                <div>
-                  <Image src="/icons/abdul.png" style={{width: "30px", height: "30px", borderRadius: "50%"}}/>
-                  <span style={{color: "white", marginLeft: "10px"}}>abdul_h</span>
-                </div>
-                <div style={{marginLeft: "15px", color: "#FFFFFF", flexWrap: "wrap"}}>
-                  <div style={{color: "#ABABAB", marginLeft: "25px"}}>Nice Place</div>
-                </div>
-              </div>
-              <div style={{display: "flex", flexDirection: "column", marginRight: "20px", marginTop: "20px"}}>
-                <div>
-                  <Image src="/images/egi.png" style={{width: "30px", height: "30px", borderRadius: "50%"}}/>
-                  <span style={{color: "white", marginLeft: "10px"}}>egi_lol</span>
-                </div>
-                <div style={{marginLeft: "15px", color: "#FFFFFF", flexWrap: "wrap"}}>
-                  <div style={{color: "#ABABAB", marginLeft: "25px"}}>Good Vibe</div>
-                </div>
+              <div style={{ height: "400px", overflow: "auto" }}>
+                {comments?.map((item) => {
+                    return(
+                      <div key={item.id} style={{display: "flex", flexDirection: "column", marginRight: "20px", marginTop: "20px"}}>
+                        <div>
+                          <Image src={"http://localhost:5000/uploads/" + item.commentor.image} style={{width: "30px", height: "30px", borderRadius: "50%"}}/>
+                          <span style={{color: "white", marginLeft: "10px"}}>{item.commentor.fullName}</span>
+                        </div>
+                        <div style={{marginLeft: "15px", color: "#FFFFFF", flexWrap: "wrap"}}>
+                          <div style={{color: "#ABABAB", marginLeft: "25px"}}>{item.comment}</div>
+                        </div>
+                      </div>
+                    )
+                  })
+                }
               </div>
               <div>
-
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group className="mb-3 input-comment" controlId="exampleForm.ControlInput1">
+                    <Form.Control style={{
+                        width: "94%",
+                        backgroundColor: "#5C5C5C",
+                        color: "#FFFFFF"
+                      }} 
+                      type="text"
+                      name="comment"
+                      value={form.comment}
+                      onChange={handleChange}
+                      placeholder="Comment..." />
+                  </Form.Group>
+                </Form>
               </div>
             </Col>
           </Row>
